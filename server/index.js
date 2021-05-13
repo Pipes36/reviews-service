2f1fa97f98db01cd71271bce9517e9c96dce211d
Pongsak Pattamasaevi <ppattamasaevi@gmail.com>
2021-05-14 00:29:41 +0800

    Added server route for serving loaderio.txt file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const reviews = require('./routes/reviewsRoutes.js');
 const meta = require('./routes/metaRoutes.js');
 
@@ -12,7 +13,9 @@ app.use(express.json());
 
 app.use('/reviews/meta', meta);
 app.use('/reviews', reviews);
-
+app.get('/loaderio-2ae007f3d5a6844c03745ca34aa724a2/', (req, res) => {
+  res.sendFile(path.resolve('./server/loaderio.txt'));
+});
 
 
 app.listen(PORT, () => {
